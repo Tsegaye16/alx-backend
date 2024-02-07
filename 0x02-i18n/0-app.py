e301e5f9c97ffd29e6f9712c455cf37db3409553
Tsegaye16 <abewatsegaye16@gmail.com>
2024-02-07 00:09:27 +0000

    python flask

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+Setup a basic Flask app
+"""
+from flask import Flask
+from flask import render_template
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    """
+    Returning a basic html page
+    """
+    return render_template('0-index.html')
+
+
+if __name__ == '__main__':
+    app.run()
