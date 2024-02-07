1980b8d459b80de1c0b1bcbd52e14b329a015506
Tsegaye16 <abewatsegaye16@gmail.com>
2024-02-07 00:24:54 +0000

    python flask

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+"""
+Setup a basic flask application
+"""
+from flask import Flask
+from flask import render_template
+from flask_babel import Babel
+
+
+class Config(object):
+    """
+    creating Config class that has a LANGUAGES
+    class attribute equal to ["en", "fr"].
+    """
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+"""
+instantiate the Babel object in your app
+"""
+app = Flask(__name__)
+app.config.from_object(Config)
+
+"""
+Wrapping the application with Babel
+"""
+babel = Babel(app)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    """
+    Returning a basic html page
+    """
+    return render_template('1-index.html')
+
+
+if __name__ == '__main__':
+    app.run()
