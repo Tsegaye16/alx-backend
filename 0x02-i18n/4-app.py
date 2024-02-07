c70a587d8ea3250e1d04ae35729163eaaff594a4
Tsegaye16 <abewatsegaye16@gmail.com>
2024-02-07 01:19:02 +0000

    python flask

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -30,13 +30,6 @@ def get_locale():
     """
     Gets locale from request object
     """
-    return request.accept_languages.best_match(app.config['LANGUAGES'])
-
-@babel.localeselector
-def get_locale():
-    """
-    Gets locale from request object
-    """
     locale = request.args.get('locale', '').strip()
     if locale and locale in Config.LANGUAGES:
         return locale
@@ -48,7 +41,7 @@ def index():
     """
     Renders a basic html template
     """
-    return render_template('2-index.html')
+    return render_template('4-index.html')
 
 
 if __name__ == '__main__':
